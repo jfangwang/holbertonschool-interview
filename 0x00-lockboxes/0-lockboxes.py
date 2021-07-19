d5ab1fa3c2e4e4441c78d5c42a8f1d7c337d7a22
jfangwang <qbs1864@gmail.com>
2021-07-19 14:24:39 +0000

    lockboxes done

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Lockboxes"""
+
+def canUnlockAll(boxes):
+    num_boxes = len(boxes)
+    checked_boxes = ['False'] * num_boxes
+    checked_boxes[0] = 'True'
+    results = check_box(boxes[0], checked_boxes, boxes)
+    for a in results:
+        if a is "False":
+            return False
+    return True
+
+"""
+Recursion
+    Input: List of keys in a box
+    Output: None, updates checked_boxes list
+"""
+def check_box(box, checked_boxes, boxes):
+    for key in box:
+        if checked_boxes[key] is "False":
+            checked_boxes[key] = "True"
+            check_box(boxes[key], checked_boxes, boxes)
+    return checked_boxes
