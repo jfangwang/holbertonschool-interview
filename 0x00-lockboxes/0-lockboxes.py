b95ef4f55cf06752122501529ca3a4e0ae46858e
jfangwang <qbs1864@gmail.com>
2021-07-21 05:45:47 +0000

    changed from recursion to for loops

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,6 +4,10 @@ Lockboxes
     Duration of Attempt:
     #1: 52 mins
         Notes: Didn't really know how to structure the recursion exactly
+    #2: 72 mins
+        Note: Recursion method did not pass checker, don't know why. Chose
+        to do it with 2 for loops. Alse changed checked_boxes to int list
+        instead of bool list
 """
 
 
@@ -11,20 +15,10 @@ def canUnlockAll(boxes):
     if boxes is None or len(boxes) == 0:
         return False
     checked_boxes = [0]
-    if len(check_box(boxes[0], checked_boxes, boxes)) != len(boxes):
+    for box in checked_boxes:
+        for key in boxes[box]:
+            if key < len(boxes) and key not in checked_boxes:
+                checked_boxes.append(key)
+    if len(checked_boxes) != len(boxes):
         return False
     return True
-
-"""
-Recursion
-    Input: List of keys in a box
-    Output: None, updates checked_boxes list
-"""
-
-
-def check_box(box, checked_boxes, boxes):
-    for key in box:
-        if key < len(boxes) and key not in checked_boxes:
-            checked_boxes.append(key)
-            check_box(boxes[key], checked_boxes, boxes)
-    return checked_boxes
