f41cc182e6b8a830cd0b87368a315c0911181e1a
jfangwang <qbs1864@gmail.com>
2021-08-26 14:08:19 -0400

    done

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -7,9 +7,41 @@
  * @head: head of the list
  * Return: 0 or 1 if it is a palindrome
  */
-int is_palindrome(listint_t **head) {
-	if (head) {
+int is_palindrome(listint_t **head)
+{
+	/* Find length of list */
+	listint_t *node;
+	int len = 1;
+
+	if (head == NULL)
 		return (0);
+	node = malloc(sizeof(listint_t));
+	node = *head;
+
+
+	while (node->next != NULL)
+	{
+		node = node->next;
+		len++;
 	}
-	return (0);
+	node = *head;
+	int arr[len];
+
+	len--;
+	while (len >= 0)
+	{
+		arr[len] = node->n;
+		node = node->next;
+		len--;
+	}
+	node = *head;
+	len++;
+	while (node)
+	{
+		if (arr[len] != node->n)
+			return (0);
+		node = node->next;
+		len++;
+	}
+	return (1);
 }
