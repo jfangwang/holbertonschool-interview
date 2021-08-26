bf573a2d11899726e5b7e19d583f7f95782cc4d9
jfangwang <qbs1864@gmail.com>
2021-08-26 13:10:06 -0400

    set up files complete, time to start palindrome

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - Check if single linked list is a palindrome
+ * @head: head of the list
+ * Return: 0 or 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head) {
+	if (head) {
+		return (0);
+	}
+	return (0);
+}
