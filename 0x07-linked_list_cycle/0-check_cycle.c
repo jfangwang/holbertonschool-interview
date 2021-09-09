0937ae3b2a9fa9301d4ae46e82acb1eecf4b5584
jfangwang <qbs1864@gmail.com>
2021-09-09 19:47:01 -0400

    initial commit

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - Check whether linked list is a cycle or not
+ * @list: Linked list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+*/
+int check_cycle(listint_t *list)
+{
+	return (list);
+}
