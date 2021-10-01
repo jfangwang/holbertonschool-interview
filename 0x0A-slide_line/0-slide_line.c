baded6a855761855939e857876d4bb301534a917
Jonny Wang <qbs1864@gmail.com>
2021-09-30 20:34:53 -0400

    Betty

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -10,69 +10,93 @@
   * Return: 1 success, 0 failure
 */
 
-static void print_array(int const *array, size_t size)
-{
-    size_t i;
-
-    printf("Line: ");
-    for (i = 0; i < size; i++)
-        printf("%s%d", i > 0 ? ", " : "", array[i]);
-    printf("\n");
-}
-
 int slide_line(int *line, size_t size, int direction)
 {
-	size_t start = 0;
-	size_t scout = 0;
-	size_t match_num = 0;
+	size_t placement, count = 0;
 
 	if (direction == SLIDE_LEFT)
 	{
-		for (start = 1; start < size; start++)
+		shift_all_left(line, size);
+		for (placement = count; placement < size; placement++)
 		{
-			print_array(line, size);
-			scout = start;
-			if (line[start - 1] != 0)
+			if (placement + 1 < size && line[placement] == line[placement + 1])
 			{
-				// If new prospect is not a 0
-				match_num = (size_t)line[start - 1];
-			} else {
-				// Find the next num and use it for match num
-				while (scout < size) {
-					if (line[scout] != 0)
-					{
-						match_num = (size_t)line[scout];
-						line[start - 1] = line[scout];
-						line[scout] = 0;
-						scout = size;
-						printf("%ls", line);
-					}
-					scout = scout + 1;
-				}
-				if (match_num == 0)
-					return (1);
-			}
-			scout = start;
-			printf("%ld \n", match_num);
-			while (scout < size)
-			{
-				// Look for the next matching num
-				if (line[scout] != 0)
-				{
-					if ((size_t)line[scout] == match_num)
-					{
-						line[start - 1] = line[scout] * 2;
-						line[scout] = 0;
-					}	
-				}
-				scout = scout + 1;
+				line[placement] *= 2;
+				line[placement + 1] = 0;
 			}
 		}
-		return (1);
+		shift_all_left(line, size);
 	}
 	else if (direction == SLIDE_RIGHT)
 	{
-		return (1);
+		shift_all_right(line, size);
+		for (placement = size; placement > 0; placement--)
+		{
+			if (placement - 2 >= 0 && line[placement - 1] == line[placement - 2])
+			{
+				line[placement - 2] *= 2;
+				line[placement - 1] = 0;
+			}
+		}
+		shift_all_right(line, size);
 	}
 	return (1);
+}
+
+/**
+  * shift_all_left - Shifts all numbers to the left
+  * @line: Pointer to an array of integers
+  * @size: Number of elements
+  * Return: Void
+*/
+
+void shift_all_left(int *line, size_t size)
+{
+	size_t scout, placement = 0;
+
+	for (placement = 0; placement < size; placement++)
+	{
+		/* Shift all numbers to the left */
+		if (line[placement] == 0)
+		{
+			scout = placement;
+			while (line[scout] == 0)
+				scout++;
+			if (scout < size)
+			{
+				line[placement] = line[scout];
+				line[scout] = 0;
+			}
+		}
+	}
+}
+
+/**
+  * shift_all_right - Shifts all numbers to the right
+  * @line: Pointer to an array of integers
+  * @size: Number of elements
+  * Return: Void
+*/
+
+void shift_all_right(int *line, size_t size)
+{
+	int scout, placement = 0;
+
+	for (placement = size; placement > 0; placement--)
+	{
+		/* Shift all numbers to the right */
+		placement--;
+		if (line[placement] == 0)
+		{
+			scout = placement;
+			while (line[scout] == 0)
+				scout--;
+			if (scout > -1)
+			{
+				line[placement] = line[scout];
+				line[scout] = 0;
+			}
+		}
+		placement++;
+	}
 }
