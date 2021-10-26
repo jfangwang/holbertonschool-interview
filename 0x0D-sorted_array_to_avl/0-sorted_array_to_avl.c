cc52c1f50173c12942fc1874e46c11ad467cd25e
jfangwang <qbs1864@gmail.com>
2021-10-26 19:53:24 -0400

    adding avl trees

---
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+
+/**
+ * sorted_array_to_avl - Function that builds an AVL tree from an array
+ * @array: int array
+ * @size: size_t
+ * Return: void, prints out the avl tree
+ */
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+    if (!array)
+        return (NULL);
+    return (array);
+}
