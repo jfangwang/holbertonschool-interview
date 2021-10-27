ea5eba197d008f02eaff3e7c2583d48ae1d7777f
jfangwang <qbs1864@gmail.com>
2021-10-26 21:17:46 -0400

    saving changes, going to change logic since we are limited to 3 functions and 40 lines per functions

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -11,7 +11,57 @@
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-    if (!array)
-        return (NULL);
-    return (array);
+	if (!array || !size)
+		return (NULL);
+	return (add_to_avl(array, size, 0, NULL));
+}
+
+avl_t *add_to_avl(int *array, size_t size, size_t index, avl_t *root)
+{
+	avl_t *new, *scout;
+
+	if (!new)
+		return (NULL);
+	/* Base Case */
+	if (index == size)
+		return (root);
+
+	/* Init new node */
+	new = malloc(sizeof(avl_t));
+	scout = malloc(sizeof(avl_t));
+	if (!new || !scout)
+		return (NULL);
+	new->n = array[index];
+
+	if (root == NULL)
+	{
+		/* This is the first node */
+		new->parent = NULL;
+		new->left = NULL;
+		new->right = NULL;
+	}
+	else
+	{
+		scout = root;
+		/* Traverse through the tree and find the leaf with no children */
+		while (scout->left != NULL && scout->right != NULL)
+		{
+			if (new->n < scout->n)
+				scout = scout->left;
+			else
+				scout = scout->right;
+		}
+		/* Add the new node to the tree */
+		new->parent = scout;
+		if (new->n < scout->n)
+			scout->left = new;
+		if (new->n < scout->n)
+			scout->right = new;
+	}
+
+	/* check if tree is balanced */
+
+	/* If false, balance the tree */
+
+	add_to_avl(array, size, index + 1, root);
 }
--- 0x0D-sorted_array_to_avl/binary_trees.h
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -21,5 +21,7 @@ typedef struct binary_tree_s
 
 void binary_tree_print(const binary_tree_t *);
 typedef struct binary_tree_s avl_t;
+avl_t *sorted_array_to_avl(int *array, size_t size);
+avl_t *add_to_avl(int *array, size_t size, size_t index, avl_t *root)
 
 #endif /* _BINARY_TREES_H_ */
