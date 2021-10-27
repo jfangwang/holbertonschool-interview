5c9c289f90b02dbc14730f0ee87b31e1fd648344
jfangwang <qbs1864@gmail.com>
2021-10-26 23:51:49 -0400

    didn't see the array was sorted when given, changed the entire code

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -6,62 +6,60 @@
 /**
  * sorted_array_to_avl - Function that builds an AVL tree from an array
  * @array: int array
- * @size: size_t
- * Return: void, prints out the avl tree
+ * @size: size_t size
+ * Return: Pointer of new node or NULL on failure
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
 	if (!array || !size)
 		return (NULL);
-	return (add_to_avl(array, size, 0, NULL));
+	return (build_avl_tree(array, NULL, 0, size - 1));
 }
 
-avl_t *add_to_avl(int *array, size_t size, size_t index, avl_t *root)
+/**
+ * build_avl_tree - Function that builds an AVL tree from an array
+ * @array: int array
+ * @root: root of AVL Tree
+ * @start: size_t of starting index of array
+ * @end: size_t of ending index of array
+ *  Return: Pointer of new node or NULL on failure
+ */
+avl_t *build_avl_tree(int *array, avl_t *root, size_t start, size_t end)
 {
-	avl_t *new, *scout;
+	size_t middle;
+	avl_t *new;
 
-	if (!new)
+	/* If Start > End, the current index has no children */
+	if (start > end)
 		return (NULL);
-	/* Base Case */
-	if (index == size)
-		return (root);
+	middle = (start + end) / 2;
+	new = add_new_node(array, middle, root);
+	/* Recurse the left and right paths of the current node */
+	if (middle < end)
+		new->right = build_avl_tree(array, new, middle + 1, end);
+	if (middle > start)
+		new->left = build_avl_tree(array, new, start, middle - 1);
+	return (new);
+}
 
-	/* Init new node */
-	new = malloc(sizeof(avl_t));
-	scout = malloc(sizeof(avl_t));
-	if (!new || !scout)
+/**
+ * add_new_node - Function that builds an AVL tree from an array
+ * @array: int array
+ * @index: int index in array
+ * @parent: Parent node of new node
+ * Return: returns the newly added node or NULL if node failed to create
+ */
+avl_t *add_new_node(int *array, int index, avl_t *parent)
+{
+	avl_t *new_node;
+
+	new_node = malloc(sizeof(avl_t));
+	if (!new_node)
 		return (NULL);
-	new->n = array[index];
-
-	if (root == NULL)
-	{
-		/* This is the first node */
-		new->parent = NULL;
-		new->left = NULL;
-		new->right = NULL;
-	}
-	else
-	{
-		scout = root;
-		/* Traverse through the tree and find the leaf with no children */
-		while (scout->left != NULL && scout->right != NULL)
-		{
-			if (new->n < scout->n)
-				scout = scout->left;
-			else
-				scout = scout->right;
-		}
-		/* Add the new node to the tree */
-		new->parent = scout;
-		if (new->n < scout->n)
-			scout->left = new;
-		if (new->n < scout->n)
-			scout->right = new;
-	}
-
-	/* check if tree is balanced */
-
-	/* If false, balance the tree */
-
-	add_to_avl(array, size, index + 1, root);
+	/* Init node */
+	new_node->n = array[index];
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	return (new_node);
 }
--- 0x0D-sorted_array_to_avl/binary_trees.h
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -22,6 +22,8 @@ typedef struct binary_tree_s
 void binary_tree_print(const binary_tree_t *);
 typedef struct binary_tree_s avl_t;
 avl_t *sorted_array_to_avl(int *array, size_t size);
-avl_t *add_to_avl(int *array, size_t size, size_t index, avl_t *root)
+avl_t *build_avl_tree(int *array, avl_t *root, size_t start, size_t end);
+avl_t *add_new_node(int *array, int index, avl_t *parent);
+
 
 #endif /* _BINARY_TREES_H_ */
