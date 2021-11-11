fcbd0c38b53746c2175ecdac6626db978be61a4f
jfangwang <qbs1864@gmail.com>
2021-11-11 01:18:50 -0500

    semistandard compliant

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -3,19 +3,24 @@
 
 const userInput = process.argv[2];
 const requestFilm = require('request');
-requestFilm('https://swapi-api.hbtn.io/api/films/' + userInput, function (error, response, body) {
-  console.error(error);
+requestFilm('https://swapi-api.hbtn.io/api/films/' + userInput, async function (error, response, body) {
+  if (error) {
+    // pass
+  }
   if (response.statusCode === 200) {
     const filmJson = JSON.parse(body);
-    filmJson.characters.forEach(character => {
-      const requestCharacter = require('request');
-      requestCharacter(character, function (error, response, body) {
-        console.error(error);
-        if (response.statusCode === 200) {
-          const characterJson = JSON.parse(body);
-          console.log(characterJson.name);
-        }
+    for (const character in filmJson.characters) {
+      await new Promise(function (resolve, reject) {
+        requestFilm(filmJson.characters[character], function (error, response, body) {
+          if (error) {
+            // pass
+          }
+          if (response.statusCode === 200) {
+            console.log(JSON.parse(body).name);
+            resolve();
+          }
+        });
       });
-    });
+    }
   }
 });
