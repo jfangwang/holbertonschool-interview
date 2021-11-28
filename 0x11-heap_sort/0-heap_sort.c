0bd057b109b12b89b942a528f5cf43d6ab17e9de
jfangwang <qbs1864@gmail.com>
2021-11-28 16:58:02 -0500

    first attempt

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -3,12 +3,54 @@
 #include "sort.h"
 
 /**
- * heap_sort - sorts an array of integers in ascending order using Heap Sort
- * @list: array
- * @size: size of array
- * Return: 0 if there is no cycle, 1 if there is a cycle
-*/
+ * heap_sort - sorts an array in heap sort order
+ * @array: input list
+ * @size: size of list
+ * Return: Nothing, void function
+ */
 void heap_sort(int *array, size_t size)
 {
-	print_array(array, size);
+	int i = 0;
+	int temp = 0;
+
+	for (i = (int)size / 2 - 1; i >= 0; i--)
+		heapify(array, (int)size, size, i);
+	for (i = (int)size - 1; i > 0; i--)
+	{
+		temp = array[i];
+		array[i] = array[0];
+		array[0] = temp;
+		print_array(array, size);
+		heapify(array, i, size, 0);
+	}
+}
+
+/**
+ * heapify - recursion of heap sorting array
+ * @array: list of ints
+ * @n: int, size of current subtree
+ * @size: size_t, size of array
+ * @i: int, index of root node
+ * Return: Nothing, void function
+ */
+void heapify(int *array, int n, size_t size, int i)
+{
+	int largest = i;
+	int left = 2 * i + 1;
+	int right = 2 * i + 2;
+	int temp;
+
+	/* Find if left or right node is smaller */
+	if (right < n && array[right] > array[largest])
+		largest = right;
+	if (left < n && array[left] > array[largest])
+		largest = left;
+	if (largest != i)
+	{
+		temp = array[largest];
+		array[largest] = array[i];
+		array[i] = temp;
+		print_array(array, size);
+		heapify(array, n, size, largest);
+	}
 }
--- 0x11-heap_sort/sort.h
+++ 0x11-heap_sort/sort.h
@@ -5,5 +5,6 @@
 
 void print_array(const int *array, size_t size);
 void heap_sort(int *array, size_t size);
+void heapify(int *array, int n, size_t size, int i);
 
 #endif /* SORT_H */
