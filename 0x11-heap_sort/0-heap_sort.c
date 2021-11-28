99cdbce99fec8a73d4a8b53d6c9513492a5ca238
jfangwang <qbs1864@gmail.com>
2021-11-28 11:42:10 -0500

    everything pushed

---
+++ 0x11-heap_sort/0-heap_sort.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * heap_sort - sorts an array of integers in ascending order using Heap Sort
+ * @list: array
+ * @size: size of array
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+*/
+void heap_sort(int *array, size_t size)
+{
+	print_array(array, size);
+}
