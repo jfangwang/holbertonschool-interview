50b60f01275eacfa34b6667cb9dc0a1e5f645db3
jfangwang <qbs1864@gmail.com>
2021-12-02 18:14:07 -0500

    done

---
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * advanced_binary - Searches for a value in a sorted array of integers.
+ * @array: input list
+ * @size: size of list
+ * @value: target number to look for in list
+ * Return: -1 if value is not found, otherwise return index of value.
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	if (array == NULL)
+		return (-1);
+
+	int left = 0;
+	int right = size - 1;
+	int mid = 0;
+
+	while (left <= right)
+	{
+		print_list(array, left, right);
+		mid = left + ((right - left) / 2);
+		if (left == right && left == mid)
+		{
+			if (array[mid] == value)
+				return (mid);
+			return (-1);
+		}
+		if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+	if (array[mid] == value)
+		return (mid);
+	return (-1);
+
+}
+
+/**
+ * print_list - Prints a list of numbers from left to right
+ * @array: input list
+ * @left: index of left
+ * @right: index of right
+ * Return: Nothing, void function
+ */
+void print_list(int *array, int left, int right)
+{
+	int i = left;
+
+	printf("Searching in array:");
+	for (i = left; i < right; i++)
+		printf(" %d,", array[i]);
+	printf(" %d", array[right]);
+	printf("\n");
+
+}
---
+++ 0x12-advanced_binary_search/search_algos.h
@@ -0,0 +1,8 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+#include <stdlib.h>
+
+int advanced_binary(int *array, size_t size, int value);
+void print_list(int *array, int left, int right);
+
+#endif /* SEARCH_ALGOS_H */
