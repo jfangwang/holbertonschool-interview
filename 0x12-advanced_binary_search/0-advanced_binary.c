c93199073e31d7aa7e74906a3b05ddd076b6391d
jfangwang <qbs1864@gmail.com>
2021-12-02 19:32:47 -0500

    got rid of extra if

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -51,8 +51,6 @@ int recurse(int *array, int left, int right, int value)
 		return (-1);
 	printf("Searching in array:");
 	print_list(array, left, right);
-	if (array[mid] == value && left <= right && left == mid)
-		return (mid);
 	if (array[mid] == value && array[mid - 1] != value)
 		return (mid);
 	if (array[mid] >= value)
