56a4a744be2db7e49c48f65d20c829b1547efe07
Jonny Wang <qbs1864@gmail.com>
2021-12-16 19:20:06 +0000

    initial starting files

---
+++ 0x14-heap_extract/0-heap_extract.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+
+/**
+ * heap_extract - extracts the root node of a Max Binary Heap
+ * @root: root node
+ * Return: 0 on failure, else the value stored in the root node
+ */
+int heap_extract(heap_t **root)
+{
+	if (root)
+		return (0);
+	return (0);
+}
