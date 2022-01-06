2909732f99cbc630e7b2039f314aad842bc391b8
Jonny Wang <qbs1864@gmail.com>
2022-01-06 20:04:08 +0000

    4th attempt

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,30 +3,12 @@
 
 
 def rotate_2d_matrix(matrix):
-    for i in range(0, round(len(matrix) / 2)):
-        nums = []
-        # Top Row
-        nums.append(matrix[0][i])
-
-        # Right Column
-        nums.append(matrix[i][len(matrix) - 1])
-        matrix[i][len(matrix) - 1] = nums.pop(0)
-
-        # Bottom Row
-        nums.append(matrix[len(matrix) - 1][len(matrix) - 1 - i])
-        matrix[len(matrix) - 1][len(matrix) - 1 - i] = nums.pop(0)
-
-        # Left Column
-        nums.append(matrix[len(matrix) - 1 - i][0])
-        matrix[len(matrix) - 1 - i][0] = nums.pop(0)
-
-        # Replace Top Row
-        matrix[0][i] = nums.pop(0)
-
-# [1, 2, 3],
-# [4, 5, 6],
-# [7, 8, 9]]
-
-# [7, 2, 1], 
-# [4, 5, 6], 
-# [9, 8, 3]
+    new_matrix = []
+    for col in range(0, len(matrix)):
+        temp = []
+        for row in range(len(matrix) - 1, -1, -1):
+            temp.append(matrix[row][col])
+        new_matrix.append(temp)
+    for row in range(0, len(matrix)):
+        for col in range(0, len(matrix)):
+            matrix[row][col] = new_matrix[row][col]
