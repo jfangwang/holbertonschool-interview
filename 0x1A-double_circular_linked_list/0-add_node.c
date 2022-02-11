02f2070dc784a121bb8f5389a18552b5e24c04e5
Jonny Wang <qbs1864@gmail.com>
2022-02-11 00:48:42 +0000

    created initial files

---
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+
+
+/**
+ * add_node_end - Add a new node to the end of a double circular linked list
+ * @list:  the list to modify
+ * @str: The string to copy into the new node
+ * Return: Address of the new node, or NULL on failure
+ */
+List *add_node_end(List **list, char *str)
+{
+    if (!list)
+        return (NULL);
+    if (!str)
+        return (NULL);
+    return (NULL);
+}
+
+
+/**
+ * add_node_end - Add a new node to the end of a double circular linked list
+ * @list:  the list to modify
+ * @str: The string to copy into the new node
+ * Return: Address of the new node, or NULL on failure
+ */
+List *add_node_begin(List **list, char *str)
+{
+	if (!list)
+        return (NULL);
+    if (!str)
+        return (NULL);
+    return (NULL);
+}
---
+++ 0x1A-double_circular_linked_list/README.md
@@ -0,0 +1,1 @@
+Welcome to Circular Linked List!
