093b1822deb3e757bf0044f6612e6872cc33fd4a
jfangwang <qbs1864@gmail.com>
2022-02-17 19:20:59 -0500

    betty

--- 0x1B-radix_sort/0-radix_sort.c
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -9,54 +9,68 @@
  */
 void radix_sort(int *array, size_t size)
 {
-    int i = 0;
-    int j = 0;
-    int max = 0;
-    int swap_index = 0;
-    int len = size;
-    int placement = 1;
+	int i = 0;
+	int j = 0;
+	int max = 0;
+	int swap_index = 0;
+	int len = size;
+	int placement = 1;
 
 
-    if (size == 0)
-        return;
-    while (i < len) {
-        if (array[i] > max)
-            max = array[i];
-        i = i + 1;
-    }
-    swap_index = 0;
-    while(max > 0) {
-        swap_index = 0;
-        for(i=0; i<10; i++) {
-            for(j=0; j<len; j++) {
-                if ((array[j] % (placement * 10)) / placement == i) {
-                    swap(array, array[j], array[swap_index], len);
-                    swap_index = swap_index + 1;
-                }
-            }
-        }
-        placement *= 10;
-        print_array(array, len);
-        max = max / 10;
-    }
+	if (size == 0)
+		return;
+	while (i < len)
+	{
+		if (array[i] > max)
+			max = array[i];
+		i += 1;
+	}
+	swap_index = 0;
+	while (max > 0)
+	{
+		swap_index = 0;
+		for (i = 0; i < 10; i++)
+		{
+			for (j = 0; j < len; j++)
+			{
+				if ((array[j] % (placement * 10)) / placement == i)
+				{
+					swap(array, array[j], array[swap_index], len);
+					swap_index = swap_index + 1;
+				}
+			}
+		}
+		placement *= 10;
+		print_array(array, len);
+		max /= 10;
+	}
 }
 
 
+/**
+ * swap - swaps 2 nums in arr.
+ * @array: The array to modify
+ * @num1: int
+ * @num2: int
+ * @size: size of arr
+ * Return: Array
+ */
 int *swap(int *array, int num1, int num2, int size)
 {
-    int i = 0;
+	int i = 0;
 
-    while (i < size) {
-        if (array[i] == num1) {
-            array[i] = num2;
-            i = i + 1;
-            continue;
-        }
-        if (array[i] == num2) {
-            array[i] = num1;
-        }
-        i = i + 1;
-    }
-    return (array);
+	while (i < size)
+	{
+		if (array[i] == num1)
+		{
+			array[i] = num2;
+			i += 1;
+			continue;
+		}
+		if (array[i] == num2)
+		array[i] = num1;
+		i += 1;
+	}
+	return (array);
 
 }
