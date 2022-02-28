7b9c43b5cc67b1314e42db494639084722f84b29
jfangwang <qbs1864@gmail.com>
2022-02-28 18:27:24 -0500

    init files

---
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_is_avl - Sorts AVL Tree
+ * @tree: root node
+ * Return: node
+ */
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	if (tree)
+		return (1);
+	return (1);
+}
