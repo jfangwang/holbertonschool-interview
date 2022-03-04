62a9f1c25e0c743a58b3c704063c5134211db69d
jfangwang <qbs1864@gmail.com>
2022-03-03 23:55:28 -0500

    working

--- 0x1D-avl_trees/0-binary_tree_is_avl.c
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -8,7 +8,7 @@
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (!tree)
 		return (0);
 	if (check_bst(tree, INT_MIN, INT_MAX) == 0)
 		return (0);
@@ -26,7 +26,7 @@ int isBalanced(const binary_tree_t *tree)
 	int left = 0;
 	int right = 0;
 
-	if (tree == NULL)
+	if (!tree)
 		return (1);
 	left = check_height(tree->left);
 	right = check_height(tree->right);
@@ -67,7 +67,7 @@ int check_height(const binary_tree_t *tree)
  */
 int check_bst(const binary_tree_t *tree, int min, int max)
 {
-	if (tree == NULL)
+	if (!tree)
 		return (1);
 	if ((tree->left && tree->left->n <= min) ||
 		(tree->right && tree->right->n >= max))
