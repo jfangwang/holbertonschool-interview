16e97edd7668610506ef27db95c1b8b5245cea59
jfangwang <qbs1864@gmail.com>
2022-04-07 20:17:37 -0400

    first attempt

---
+++ 0x22-primegame/0-prime_game.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Prime Game"""
+
+
+def isWinner(x, nums):
+    """isWinner"""
+    ben = 0
+    maria = 0
+    for i in range(0, x):
+        prime = []
+
+        # find prime numbers
+        for j in range(1, nums[i] + 1):
+            isPrime = True
+            for k in range(2, j):
+                if (j % k == 0):
+                    isPrime = False
+            if (isPrime):
+                prime.append(j)
+        if (len(prime) % 2 == 0):
+            maria += 1
+        else:
+            ben += 1
+    if (ben > maria):
+        return ('Ben')
+    return ('Maria')
